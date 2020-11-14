fe680e687ce750bd3f9a97813efab48d5ec4a913
Szymon Rut <szymon.rut@unitedideas.pl>
2020-11-14 16:09:05 +0100

    Add contactships and contacts associations in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :contactships, ->(user) { ContactshipsQuery.both_ways(user_id: user.id) }, inverse_of: :user, dependent: :destroy
+  has_many :contacts, ->(user) { UsersQuery.contacts(user_id: user.id, scope: true) }, through: :contactships
+
   validates :email, presence: true, uniqueness: { case_sensitive: true }, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   validates :name, presence: true
   validates :surname, presence: true
--- spec/support/user_examples.rb
+++ spec/support/user_examples.rb
@@ -1,7 +1,36 @@
 require 'spec_helper'
 
 shared_examples 'User' do
+  let(:agents) { create_list(:agent, 2) }
+  let(:operators) { create_list(:operator, 2) }
+
   describe 'via relations' do
+    it { is_expected.to have_many(:contactships).inverse_of(:user).dependent(:destroy) }
+    it { is_expected.to have_many(:contacts).through(:contactships) }
+
+    context 'should return contacts bidirectional' do
+      before do
+        agents[0].contacts << agents[1]
+        agents[0].contacts << operators
+        agents[1].contacts << operators[1]
+      end
+
+      it 'for agent 1' do
+        expect(agents[0].contacts).to eq([agents[1], operators[0], operators[1]])
+      end
+
+      it 'for agent 2' do
+        expect(agents[1].contacts).to eq([agents[0], operators[1]])
+      end
+
+      it 'for operator 1' do
+        expect(operators[0].contacts).to eq([agents[0]])
+      end
+
+      it 'for operator 2' do
+        expect(operators[1].contacts).to eq(agents)
+      end
+    end
   end
 
   describe 'via validations' do
